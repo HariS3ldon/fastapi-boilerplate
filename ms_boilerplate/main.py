e44edb756aa532d71d5019e989107f5edb3baad3
Enrico <enrico.pascucci@hubcore.ai>
2025-11-13 17:10:10 +0000

    added alembic. works out of the box

--- ms_boilerplate/main.py
+++ ms_boilerplate/main.py
@@ -1,16 +1,14 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from .routing.v1 import router as v1_router
 
 
 from dotenv import load_dotenv
-from ms_boilerplate.config.db import engine, Base
+from config.db import engine, Base
 load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # Database initialization will be handled by Alembic migrations
     yield
 
 app = FastAPI(lifespan=lifespan)
@@ -18,6 +16,3 @@ app = FastAPI(lifespan=lifespan)
 
 from ms_boilerplate.exception_handlers import register_exception_handlers
 register_exception_handlers(app)
-app.include_router(v1_router, prefix="/v1", tags=["v1"])
-
-    
